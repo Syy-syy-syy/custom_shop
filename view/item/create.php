a8c961a6b81b7a6277a0444c9c64814107a59570
ree-km <reina.harada@artemis-tech.jp>
2020-07-14 09:42:59 +0000

    カテゴリ選択追加

--- view/item/create.php
+++ view/item/create.php
@@ -12,31 +12,52 @@ $options = $db_val['options'];
 if (!empty($_POST['create'])) {
     try {
         $pdo = new PDO($dsn, $user, $pass, $options);
-        $sql = 'INSERT INTO items(name, descript, price, stock) VALUE (:name, :descript, :price, :stock)';
+        $sql = 'INSERT INTO items (name, descript, price, stock, category_id) VALUE (:name, :descript, :price, :stock, :category_id)';
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':name', $_POST['name'], PDO::PARAM_STR);
         $stmt->bindParam(':descript', $_POST['descript'], PDO::PARAM_STR);
         $stmt->bindParam(':price', $_POST['price'], PDO::PARAM_INT);
         $stmt->bindParam(':stock', $_POST['stock'], PDO::PARAM_INT);
+        $stmt->bindParam(':category_id', $_POST['category'], PDO::PARAM_INT);
         $stmt->execute();
         $pdo = null;
 
-        header("Location: index.php");
+        header("Location: /item/index.php");
         exit();
     } catch (PDOException $e) {
         return $e->getMessage();
     }
 }
+
+$pdo = new PDO($dsn, $user, $pass, $options);
+$sql = 'SELECT * FROM categories';
+$stmt = $pdo->query($sql);
+$all = $stmt->fetchAll();
+$pdo = null;
+
+if (empty($all)) {
+    header("Location: /category/create.php");
+}
+
 ?>
 
 <h1>商品作成ページ</h1>
 
-<form action="create.php" method="post">
+<form action="/item/create.php" method="post">
     <ul>
         <li>商品名:<input type="text" name="name"></li>
         <li>商品説明:<input type="text" name="descript"></li>
         <li>価格:<input type="text" name="price"></li>
         <li>在庫数:<input type="text" name="stock"></li>
+        <li>
+            <select name="category">
+                <?php foreach($all as $category) { ?>
+                    <option value= <?php echo $category['id'] ?>>
+                    <?php echo $category['name']; ?>
+                    </option>
+                <?php } ?>
+            </select>
+        </li>
     </ul>
     <input type="submit" name="create" value="作成">
 </form>
